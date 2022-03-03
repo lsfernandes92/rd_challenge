f3ed9f41321c64d8d7a1939e35e819c7b59dbc85
lsfernandes92 <lsfernandes92@gmail.com>
2022-03-03 17:05:56 -0300

    Polish CustomerSuccessBalancing class with the new interfaces

--- ruby/lib/customer_success_balancing.rb
+++ ruby/lib/customer_success_balancing.rb
@@ -1,9 +1,9 @@
 require 'byebug'
 require_relative '../helpers/score_build_helper'
+require_relative 'manager'
+require_relative 'rate_managers'
 
 class CustomerSuccessBalancing
-  DRAW_CASE_VALUE = 0
-
   include ScoreBuildHelper
 
   def initialize(managers, customers, absent_managers)
@@ -23,44 +23,19 @@ class CustomerSuccessBalancing
   private
 
   def check_most_rated_manager
-    preffered_manager = 1
     customers_attended = []
-    max_customers_attended = 0
+    working_managers = []
 
     @managers.each do |manager_id, manager_score|
-      manager_clients = 0
-
-      @customers.except(*customers_attended).each do |customer_id, customer_score|
-        if valid_customer_for_manager?(manager_score, customer_score)
-          customers_attended << customer_id
-          manager_clients += 1
-        end
-      end
-
-      if most_rated_manager?(manager_clients, max_customers_attended)
-        max_customers_attended = manager_clients
-        preffered_manager = manager_id
-      elsif draw_case?(manager_clients, max_customers_attended)
-        preffered_manager = DRAW_CASE_VALUE
-      end
+      manager = Manager.new(manager_id, manager_score)
+      customers_attended += manager.attend_customers(@customers.except(*customers_attended))
+      working_managers << manager
     end
 
-    preffered_manager
+    RateManagers.new(working_managers).most_rated
   end
 
   private
-
-  def valid_customer_for_manager?(manager_score, customer_score)
-    manager_score >= customer_score
-  end
-
-  def most_rated_manager?(clients, max_customers_attended)
-    clients > max_customers_attended
-  end
-
-  def draw_case?(clients, max_customers_attended)
-    clients == max_customers_attended
-  end
 
   def check_managers_availability
     @managers = @managers.except(*@absent_managers)

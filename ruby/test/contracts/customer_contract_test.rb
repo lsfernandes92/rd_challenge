34f7ece9543e90003f1ee494e268c666da367075
lsfernandes92 <lsfernandes92@gmail.com>
2022-03-04 17:34:38 -0300

    Add `CustomerContract` tests

---
+++ ruby/test/contracts/customer_contract_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/contracts/customer_contract'
+
+class CustomerContractTests < Minitest::Test
+
+  def test_id_min_limit
+    @result = contract.(ids: [0], scores: [0])
+    assert_equal true, @result.failure?
+    assert_includes(result_id_errors, ["id must be between 1 and 999999"])
+  end
+
+  def test_id_max_limit
+    @result = contract.(ids: [1000000], scores: [10])
+    assert_equal true, @result.failure?
+    assert_includes(result_id_errors, ["id must be between 1 and 999999"])
+  end
+
+  def test_score_min_limit
+    @result = contract.(ids: [1], scores: [0])
+    assert_equal true, @result.failure?
+    assert_includes(result_scores_errors, ["score must be between 1 and 99999"])
+  end
+
+  def test_score_max_limit
+    @result = contract.(ids: [1], scores: [100000])
+    assert_equal true, @result.failure?
+    assert_includes(result_scores_errors, ["score must be between 1 and 99999"])
+  end
+
+  private
+
+  def contract
+    @contract ||= CustomerContract.new
+  end
+
+  def result_scores_errors
+    @result.errors.to_h[:scores].first
+  end
+
+  def result_id_errors
+    @result.errors.to_h[:ids].first
+  end
+end

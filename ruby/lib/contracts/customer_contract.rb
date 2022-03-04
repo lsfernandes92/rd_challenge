5f55830306f62fa200cdc3efd530df5d7d13ced4
lsfernandes92 <lsfernandes92@gmail.com>
2022-03-04 17:34:54 -0300

    Add `CustomerContract` class

---
+++ ruby/lib/contracts/customer_contract.rb
@@ -0,0 +1,21 @@
+require 'dry/validation'
+
+class CustomerContract < Dry::Validation::Contract
+  params do
+    optional(:ids).array(:integer)
+    optional(:scores).array(:integer)
+  end
+
+  rule(:ids).each { key.failure('id must be between 1 and 999999') if id_in_range?(value) }
+  rule(:scores).each { key.failure('score must be between 1 and 99999') if score_in_range?(value) }
+
+  private
+
+  def id_in_range?(value)
+    (value < 1 || value > 999999)
+  end
+
+  def score_in_range?(value)
+    (value < 1 || value > 99999)
+  end
+end

27b560b70ac5080da562114c9332e023c3d3fb7d
lsfernandes92 <lsfernandes92@gmail.com>
2022-03-04 17:40:44 -0300

    Implement tests for `AbsenceValidator`

---
+++ ruby/test/validators/absence_validator_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/validators/absence_validator'
+require_relative '../../helpers/score_build_helper'
+
+class AbsenceValidatorTests < Minitest::Test
+  include ScoreBuildHelper
+
+  def test_when_absent_managers_is_empty
+    validator = AbsenceValidator.new(
+      [10, 20],
+      []
+    )
+    assert validator.validate, true
+  end
+
+  def test_with_reasonable_absents
+    validator = AbsenceValidator.new(
+      [10, 20, 30, 40],
+      [1, 2]
+    )
+    assert validator.validate, true
+  end
+
+  def test_when_absents_exceeds_limit
+    validator = AbsenceValidator.new(
+      [10, 20, 30, 40],
+      [1, 2, 3]
+    )
+    exception = assert_raises(RuntimeError) { validator.validate }
+    assert_match /The managers absents is more than expected/, exception.message
+  end
+end
